9b49b1bafe80f3ca44c18269cf01c9d8d4d0b874
Viridian084 <teashen.james@gmail.com>
2020-12-15 23:36:44 +0000

    Nothing, but i want to submit this.

--- main.py
+++ main.py
@@ -18,19 +18,24 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+# Defining the employe Variable Values
 emp_1 = Employee('Teashen', 'James', 50000)
 emp_2 = Employee('Test', 'User', 25000)
-
+# Prininting the objects
 print(emp_1)
 print(emp_2)
-
+# Printing the email of each object
 print(emp_1.email)
 print(emp_2.email)
 
 print(emp_1.fullname())
 
+# Creating the Child Class 'Goons'
 class Goons(Employee):
     pass
-
+# Defining the Goons, using the defined values in the Employees Class
 goon_1 = Goons('Gruul', 'Ghoul', 'N/A')
-goon_2 = Goons('Scroth', 'Ghool', 'N/A')
+goon_2 = Goons('Scront', 'Ghool', 'N/A')
+
+print(goon_1.pay)
+print(goon_2.first)
